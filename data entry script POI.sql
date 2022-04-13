0ecf74e4e86b0268a921c2f823e1bab3ae3fedc3
chonger878 <echong06@gmail.com>
2022-04-12 23:22:53 -0700

    Updated data insertion script

--- data entry script POI.sql
+++ data entry script POI.sql
@@ -3,3 +3,10 @@ VALUES('The Machine'),
       ('Samaritan'),
       ('New York Police Department');
 
+INSERT INTO theHeroes(hFirstName,hLastName,cAffliationID)
+VALUES('Harold', 'Finch', 1),
+      ('John', 'Reese', 1),
+      ('Sameen', 'Shaw', 1),
+      ('Samantha (Root)', 'Groves', 1),
+      ('Lionel', 'Fusco', 1)
+
