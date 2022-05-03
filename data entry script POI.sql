d85b5669baa3dd2bad70b347b64e24eec8842fc9
chonger878 <echong06@gmail.com>
2022-05-03 01:04:14 -0700

    Entered more data into DB

--- data entry script POI.sql
+++ data entry script POI.sql
@@ -10,12 +10,14 @@ VALUES('Harold', 'Finch', 1),
       ('Sameen', 'Shaw', 1),
       ('Samantha (Root)', 'Groves', 1),
       ('Lionel', 'Fusco', 1),
-      ('Joss', 'Carter', 3)
+      ('Joss', 'Carter', 3),
+      ('Logan', 'Pierce', 1)
 
 INSERT INTO theVillains(vFirstName, vLastName,cAffliationID)
 VALUES('Patrick', 'Simmons', 4),
       ('Martine', 'Rousseau', 2),
-      ('John', 'Greer', 2)
+      ('John', 'Greer', 2),
+      ('Alonzo', 'Quinn', 4)
 
 INSERT INTO indivStatus(numIndivID, heroID, villainID, isDead, isAlive, isUnknown)
 VALUES(null,6, null, true, false, false, false)
